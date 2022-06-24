8a8ad91a3581630ee315403376cc36352cd1f75d
Шамиль <mamedov.shamil14@gmail.com>
2022-06-24 19:16:49 +1000

    Added staff and document analyser (syntax)

--- MDCourseProject/MDCourseSystem/DataAnalysers/StaffDataAnalyser.cs
+++ MDCourseProject/MDCourseSystem/DataAnalysers/StaffDataAnalyser.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace MDCourseProject.AppWindows.DataAnalysers;
 
@@ -11,7 +13,18 @@ public class AddValuesStaffAnalyser:DataAnalyser
     
     public override bool IsCorrectInputData()
     {
-        return false;
+        var noSyntaxErrorInName = _textBoxes[0].Text.Split().Length == 3 && _textBoxes[0].Text != string.Empty 
+                                 && _textBoxes[0].Text.Split().All(str => str.Length == 1 || str.Substring(1,str.Length - 1).All(char.IsLower))
+                                 && _textBoxes[0].Text.Split().Select(str => str.All(sym => char.IsLetter(sym) || sym == '-') && char.IsUpper(str[0])).All(checkFio => checkFio);
+        var noSyntaxErrorInOccupation = _textBoxes[1].Text.All(sym => char.IsLetter(sym) || sym is ' ' or '-') && _textBoxes[1].Text != string.Empty;
+        var noSyntaxErrorInDistrict = _textBoxes[2].Text.All(sym => char.IsLetterOrDigit(sym) || sym is '-' or '/' or '.' or ',' or ' ')
+                              && _textBoxes[2].Text != string.Empty;
+        if (!(noSyntaxErrorInName && noSyntaxErrorInOccupation && noSyntaxErrorInDistrict))
+        {
+            MessageBox.Show("Некорректные данные!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+        return noSyntaxErrorInName && noSyntaxErrorInOccupation && noSyntaxErrorInDistrict;
+
     }
 }
 
@@ -23,6 +36,14 @@ public class AddValuesDocumentsAnalyser:DataAnalyser
     
     public override bool IsCorrectInputData()
     {
-        return false;
+        var noSyntaxErrorInDocument = _textBoxes[0].Text.All(sym => char.IsLetterOrDigit(sym) || sym is '-' or ' ')
+                                      && _textBoxes[0].Text != string.Empty;
+        var noSyntaxErrorInOccupation = _textBoxes[1].Text.All(sym => char.IsLetter(sym) || sym is ' ' or '-') && _textBoxes[1].Text != string.Empty;
+        var noSyntaxErrorInDivision = _textBoxes[2].Text.All(sym => char.IsLetter(sym) || sym is ' ' or '-') && _textBoxes[2].Text != string.Empty;
+        if (!(noSyntaxErrorInDocument && noSyntaxErrorInOccupation && noSyntaxErrorInDivision))
+        {
+            MessageBox.Show("Некорректные данные!", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+        return noSyntaxErrorInDocument && noSyntaxErrorInOccupation && noSyntaxErrorInDivision;
     }
 }
