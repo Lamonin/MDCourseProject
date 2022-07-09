b8e78b89ab5e37eb9751aabfd91d107d163031ee
Maxim <maze.zte@gmail.com>
2022-07-09 12:56:40 +1000

    Rework GetHashCode for DivisionsNameAndArea

---
+++ MDCourseProject/FundamentalStructures/UsefulMethods.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace FundamentalStructures;
+
+public static class UsefulMethods
+{
+    public static int ConvertStringToNumber(string input)
+    {
+        int k = 0;
+        return 1 + input.ToLowerInvariant().Sum(c => c + k++);
+    }
+}
